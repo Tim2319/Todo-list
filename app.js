71dce4cd66edc263eca95201172668d19d517fa2
Tim2319 <destiny65740@gmail.com>
2023-09-24 17:55:27 +0800

    implement updating todo

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 
 const { engine } = require('express-handlebars')
+const methodOverride = require('method-override')
 
 const db = require('./models')
 const Todo = db.Todo
@@ -11,7 +12,8 @@ app.engine('.hbs', engine({ extname: '.hbs'}))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 
-app.use(express.urlencoded({ extname: 'true'}))
+app.use(express.urlencoded({ extended: 'true'}))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     res.render('index')
@@ -22,12 +24,9 @@ app.get('/todos', (req,res) => {
         attributes: ['id','name'],
         raw: true
     })
-        .then ((todos) => { 
-            console.log(todos)
-            return res.render('todos', { todos })
+        .then ((todos) => res.render('todos', { todos }))
         })
-    .catch((err) => res.status(422).json(err))
-})
+
 
 app.get('/todos/new', (req, res) => {
     return res.render('new')
@@ -51,11 +50,21 @@ app.get('/todos/:id', (req, res) => {
 })
 
 app.get('/todos/:id/edit', (req, res) => {
-    res.send(`get todo edit form, id:${req.params,id}`)
+    const id = req.params.id
+
+    return Todo.findByPk(id, {
+        attributes: ['id', 'name'],
+        raw: true
+    })
+        .then((todo) => res.render('edit', { todo }))
 })
 
-app.put('/todo/:id', (req, res) => {
-    res.send(`todo id:${req.params.id} has been modified`)
+app.put('/todos/:id', (req, res) => {
+    const body = req.body
+    const id = req.params.id
+    
+    return Todo.update({ name: body.name }, { where:{ id }})
+        .then(() => res.redirect(`/todos/${id}`))
 })
 
 app.delete('/todos/:id', (req,res) =>{
